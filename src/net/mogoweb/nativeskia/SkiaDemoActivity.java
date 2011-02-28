a946634f67eb929b2e96e85e07b17af107d4d0f3
mogoweb@gmail.com <mogoweb@gmail.com@54c6071b-a1e8-2f46-b62c-bb669e2e367b>
2011-02-28 10:11:13 +0000

    增加SkiaDemoActivity和SkiaView类

---
+++ src/net/mogoweb/nativeskia/SkiaDemoActivity.java
@@ -0,0 +1,18 @@
+package net.mogoweb.nativeskia;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SkiaDemoActivity extends Activity
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        SkiaView view = new SkiaView(this, Integer.parseInt(getIntent().getStringExtra("id")));
+
+        setContentView(view);
+    }
+
+}
---
+++ src/net/mogoweb/nativeskia/SkiaView.java
@@ -0,0 +1,56 @@
+package net.mogoweb.nativeskia;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.util.Log;
+import android.view.View;
+
+public class SkiaView extends View
+{
+    private static final String TAG = "nativeskia";
+
+    private int mSampleId = 0;
+
+    static
+    {
+        System.loadLibrary("nativeskia");
+    }
+
+    public native void renderHello(Canvas canvas);
+    public native void renderText(Canvas canvas);
+
+    public SkiaView(Context context)
+    {
+        super(context);
+
+        setBackgroundColor(Color.WHITE);
+    }
+
+    public SkiaView(Context context, int sampleId)
+    {
+        this(context);
+        mSampleId = sampleId;
+    }
+
+    @Override
+    public void onDraw(Canvas canvas)
+    {
+        super.onDraw(canvas);
+
+        switch (mSampleId)
+        {
+        case 0:
+            Log.d(TAG, "render hello");
+            renderHello(canvas);
+            break;
+        case 1:
+            Log.d(TAG, "render text");
+            renderText(canvas);
+            break;
+        default:
+            break;
+        }
+    }
+
+}
